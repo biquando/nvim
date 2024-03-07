6d99c6e0ee5e9efb5d2532ff56f3cd2fb283a4a1
Quan Do <biquando@gmail.com>
2024-03-07 01:12:21 -0800

    Add linting
    To add a linter, add to the config in `lazy.lua`, and install via Mason.

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -41,6 +41,25 @@ require('lazy').setup({
     },
   },
 
+  -- SECTION: Linting
+  {
+    'mfussenegger/nvim-lint',
+    config = function()
+      local lint = require('lint')
+      lint.linters_by_ft = {
+        python = {'pylint'},
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePost', 'InsertLeave'}, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
+
   -- SECTION: Autocompletion
   {
     'hrsh7th/nvim-cmp',
