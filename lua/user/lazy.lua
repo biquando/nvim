57ceee28e91c7458ffb16a176d6db657c93b1bc9
Quan Do <biquando@gmail.com>
2024-03-07 01:40:14 -0800

    Disable `import-error` for pylint
    These errors are already handled by pyright, and pylint just messes up

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -50,6 +50,11 @@ require('lazy').setup({
         python = {'pylint'},
       }
 
+      local pylint = require('lint').linters.pylint
+      pylint.args = {
+        '-f', 'json', '--disable=import-error'
+      }
+
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
       vim.api.nvim_create_autocmd({'BufEnter', 'BufWritePost', 'InsertLeave'}, {
         group = lint_augroup,
