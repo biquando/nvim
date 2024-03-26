1b8bda478250876ed4670d1fc8aec751bdca12b4
Quan Do <biquando@gmail.com>
2024-03-26 14:36:48 -0700

    Close floating oil before calling harpoon

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,8 +4,12 @@ if oil_exists then
   oil.setup({
     skip_confirm_for_simple_edits = true,
     keymaps = {
-      ['<C-h>'] = false,
-      ['<C-l>'] = false,
+      -- FIXME: it would be better to call some wrapper function instead of
+      -- calling the harpoon functions directly
+      ['<C-h>'] = function() oil.close(); require('harpoon.ui').nav_file(1) end,
+      ['<C-j>'] = function() oil.close(); require('harpoon.ui').nav_file(2) end,
+      ['<C-k>'] = function() oil.close(); require('harpoon.ui').nav_file(3) end,
+      ['<C-l>'] = function() oil.close(); require('harpoon.ui').nav_file(4) end,
       ['<Esc>'] = 'actions.close',
     },
     view_options = {
