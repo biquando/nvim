6e0f914e238f5a4b9c657da8e514c31feb18514d
Quan Do <biquando@gmail.com>
2024-03-29 20:30:55 -0700

    Add back luasnip for completions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,15 +1,43 @@
 local cmp_exists, cmp = pcall(require, 'cmp')
-if not cmp_exists then
+local luasnip_exists, luasnip = pcall(require, 'luasnip')
+if not (cmp_exists and luasnip_exists) then
   return
 end
 
+luasnip.config.setup({})
+
 cmp.setup({
+
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  completion = { completeopt = 'menu,menuone,noinsert' },
+
   mapping = cmp.mapping.preset.insert {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
-    ["<C-Space>"] = cmp.mapping.complete(),
+    ['<C-Space>'] = cmp.mapping.confirm({ select = false }),
+
+    -- Jump left in completion fields
+    ['<C-h>'] = cmp.mapping(function()
+      if luasnip.locally_jumpable(-1) then
+        luasnip.jump(-1)
+      end
+    end, { 'i', 's' }),
+
+    -- Jump right in completion fields
+    ['<C-l>'] = cmp.mapping(function()
+      if luasnip.expand_or_locally_jumpable() then
+        luasnip.expand_or_jump()
+      end
+    end, { 'i', 's' }),
   },
+
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'path' },
   },
+
 })
--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -48,7 +48,22 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = {
       -- Adds LSP completion capabilities
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets.
+          -- This step is not supported in many windows environments.
+          -- Remove the below condition to re-enable on windows.
+          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+      },
+      'saadparwaiz1/cmp_luasnip',
+
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
     },
   },
 
