d6c359348f93967c4963b30c389d96e63127f62c
Quan Do <biquando@gmail.com>
2024-03-29 21:27:59 -0700

    Remove jsregex from luasnip

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -50,15 +50,6 @@ require('lazy').setup({
       -- Adds LSP completion capabilities
       {
         'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets.
-          -- This step is not supported in many windows environments.
-          -- Remove the below condition to re-enable on windows.
-          if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
       },
       'saadparwaiz1/cmp_luasnip',
 
